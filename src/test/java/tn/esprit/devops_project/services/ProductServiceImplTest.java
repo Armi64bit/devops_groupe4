f1d87cdeada6aab64acb0f22045014b8477b0fd8
Armi64bit <bahaeddine.bouzid@esprit.tn>
2024-02-29 23:07:02 +0100

    Update ProductServiceImplTest.java

--- src/test/java/tn/esprit/devops_project/services/ProductServiceImplTest.java
+++ src/test/java/tn/esprit/devops_project/services/ProductServiceImplTest.java
@@ -7,6 +7,8 @@ import tn.esprit.devops_project.entities.Stock;
 import tn.esprit.devops_project.repositories.ProductRepository;
 import tn.esprit.devops_project.repositories.StockRepository;
 
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
@@ -15,19 +17,28 @@ class ProductServiceImplTest {
     @Test
     void addProduct() {
         // Given
-      StockRepository stockRepository = mock(StockRepository.class);
-
+        StockRepository stockRepository = mock(StockRepository.class);
         ProductRepository productRepository = mock(ProductRepository.class);
-        ProductServiceImpl productService = new ProductServiceImpl(productRepository,stockRepository);
+        ProductServiceImpl productService = new ProductServiceImpl(productRepository, stockRepository);
+
+        // Create a mock Stock object
+        Stock mockStock = new Stock();
+        mockStock.setIdStock(1); // Set necessary properties
+
+        // Set up the behavior of the stockRepository mock to return the mockStock object
+        when(stockRepository.findById(anyLong())).thenReturn(Optional.of(mockStock));
+
+        // Create a Product object
         Product productToAdd = new Product();
         productToAdd.setTitle("test");
-        Stock stock=new Stock(1);
+
         // When
-        productService.addProduct(productToAdd,stock.getIdStock());
+        productService.addProduct(productToAdd, mockStock.getIdStock());
 
         // Then
         verify(productRepository, times(1)).save(productToAdd);
     }
+
 
     @Test
     void retrieveProduct() {
