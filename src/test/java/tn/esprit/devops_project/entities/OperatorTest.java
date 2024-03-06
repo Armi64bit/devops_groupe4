072b4222ae2e5ad9dbef7abab8f3b72ede335264
balkis hmidi <balkis.hmidi@esprit.tn>
2024-03-06 20:09:24 +0100

    invoice31166

--- src/test/java/tn/esprit/devops_project/entities/OperatorTest.java
+++ src/test/java/tn/esprit/devops_project/entities/OperatorTest.java
@@ -1,14 +1,23 @@
 package tn.esprit.devops_project.entities;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.HashSet;
 import java.util.Set;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 class OperatorTest {
 
+    @Mock
+    private Invoice invoiceMock;
+
     @Test
-    void initializeInvoices() {
+    void initializeInvoices_ShouldInitializeInvoicesSet() {
         // Arrange
         Operator operator = new Operator();
 
