1f447fc00bbb88ba6d10872df6ba0382c6ebafba
balkis hmidi <balkis.hmidi@esprit.tn>
2024-03-06 19:27:10 +0100

    invoice

---
+++ src/test/java/tn/esprit/devops_project/entities/ActivitySectorTest.java
@@ -0,0 +1,40 @@
+package tn.esprit.devops_project.entities;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ActivitySectorTest {
+
+    @Test
+    void getIdSecteurActivite() {
+    }
+
+    @Test
+    void getCodeSecteurActivite() {
+    }
+
+    @Test
+    void getLibelleSecteurActivite() {
+    }
+
+    @Test
+    void getSuppliers() {
+    }
+
+    @Test
+    void setIdSecteurActivite() {
+    }
+
+    @Test
+    void setCodeSecteurActivite() {
+    }
+
+    @Test
+    void setLibelleSecteurActivite() {
+    }
+
+    @Test
+    void setSuppliers() {
+    }
+}
---
+++ src/test/java/tn/esprit/devops_project/entities/OperatorTest.java
@@ -0,0 +1,52 @@
+package tn.esprit.devops_project.entities;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OperatorTest {
+
+    @Test
+    void initializeInvoices() {
+    }
+
+    @Test
+    void getIdOperateur() {
+    }
+
+    @Test
+    void getFname() {
+    }
+
+    @Test
+    void getLname() {
+    }
+
+    @Test
+    void getPassword() {
+    }
+
+    @Test
+    void getInvoices() {
+    }
+
+    @Test
+    void setIdOperateur() {
+    }
+
+    @Test
+    void setFname() {
+    }
+
+    @Test
+    void setLname() {
+    }
+
+    @Test
+    void setPassword() {
+    }
+
+    @Test
+    void setInvoices() {
+    }
+}
