99eff236a7f939e8435aa4c0aa3f80f875f56c52
balkis hmidi <balkis.hmidi@esprit.tn>
2024-03-06 19:40:02 +0100

    invoice2

--- src/test/java/tn/esprit/devops_project/entities/ActivitySectorTest.java
+++ src/test/java/tn/esprit/devops_project/entities/ActivitySectorTest.java
@@ -1,7 +1,12 @@
 package tn.esprit.devops_project.entities;
+
 import org.junit.jupiter.api.Test;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class ActivitySectorTest {
     @Test
     void getIdSecteurActivite() {
@@ -29,5 +34,38 @@ class ActivitySectorTest {
         // Assert
         assertNotNull(code);
         assertEquals("ABC", code);
+    }
+
+    @Test
+    void constructorWithIdAndName_ShouldSetIdAndName() {
+        // Arrange
+        Long id = 1L;
+        String name = "Test Sector";
+
+        // Act
+        ActivitySector activitySector = new ActivitySector(id, name);
+
+        // Assert
+        assertNotNull(activitySector);
+        assertEquals(id, activitySector.getIdSecteurActivite());
+        assertEquals(name, activitySector.getLibelleSecteurActivite());
+    }
+
+    @Test
+    void constructorWithAllFieldsExceptId_ShouldSetFieldsCorrectly() {
+        // Arrange
+        String code = "XYZ";
+        String name = "Test Sector";
+        Set<Supplier> suppliers = new HashSet<>();
+
+        // Act
+        ActivitySector activitySector = new ActivitySector(code, name, suppliers);
+
+        // Assert
+        assertNotNull(activitySector);
+        assertNull(activitySector.getIdSecteurActivite()); // id will be generated
+        assertEquals(code, activitySector.getCodeSecteurActivite());
+        assertEquals(name, activitySector.getLibelleSecteurActivite());
+        assertEquals(suppliers, activitySector.getSuppliers());
     }
 }
