b4faa282e53f7e579434ac40c1465637dd5ecdbf
Armi64bit <80203683+Armi64bit@users.noreply.github.com>
2024-03-31 11:57:26 +0100

    Update DevOpsProjectSpringBootApplication.java

--- src/main/java/tn/esprit/devopsproject/DevOpsProjectSpringBootApplication.java
+++ src/main/java/tn/esprit/devopsproject/DevOpsProjectSpringBootApplication.java
@@ -2,18 +2,12 @@ package tn.esprit.devopsproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class DevOpsProjectSpringBootApplication {
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext context = SpringApplication.run(DevOpsProjectSpringBootApplication.class, args);
-        // You can perform additional operations with the context if needed
+        SpringApplication.run(DevOpsProjectSpringBootApplication.class, args);
     }
 
-    // Method to start the Spring Boot application
-    public static ConfigurableApplicationContext startApplication(String[] args) {
-        return SpringApplication.run(DevOpsProjectSpringBootApplication.class, args);
-    }
 }
