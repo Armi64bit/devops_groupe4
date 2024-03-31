f2b017d5b31b4d09383668ddf936984851cc5389
Armi64bit <80203683+Armi64bit@users.noreply.github.com>
2024-03-31 13:15:34 +0100

    q

---
+++ src/main/java/tn/esprit/devopsproject/ApplicationStarter.java
@@ -0,0 +1,15 @@
+package tn.esprit.devopsproject;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ApplicationStarter implements ApplicationRunner {
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        // Your initialization logic here
+    }
+
+    // Other methods and logic can be implemented here
+}
--- src/main/java/tn/esprit/devopsproject/DevOpsProjectSpringBootApplication.java
+++ src/main/java/tn/esprit/devopsproject/DevOpsProjectSpringBootApplication.java
@@ -1,5 +1,4 @@
 package tn.esprit.devopsproject;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,5 +8,4 @@ public class DevOpsProjectSpringBootApplication {
     public static void main(String[] args) {
         SpringApplication.run(DevOpsProjectSpringBootApplication.class, args);
     }
-
 }
