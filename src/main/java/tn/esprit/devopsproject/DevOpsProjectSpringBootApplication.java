664cf2ab98ea2a72981fca958243caf1529a214e
Armi64bit <80203683+Armi64bit@users.noreply.github.com>
2024-04-16 03:50:09 +0100

    granfa logs

--- src/main/java/tn/esprit/devopsproject/DevOpsProjectSpringBootApplication.java
+++ src/main/java/tn/esprit/devopsproject/DevOpsProjectSpringBootApplication.java
@@ -1,9 +1,12 @@
 package tn.esprit.devopsproject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DevOpsProjectSpringBootApplication {
+    final static Logger logger = LoggerFactory.getLogger(DevOpsProjectSpringBootApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(DevOpsProjectSpringBootApplication.class, args);
--- src/main/java/tn/esprit/devopsproject/controllers/ProductController.java
+++ src/main/java/tn/esprit/devopsproject/controllers/ProductController.java
@@ -1,6 +1,7 @@
 package tn.esprit.devopsproject.controllers;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import tn.esprit.devopsproject.entities.Product;
 import tn.esprit.devopsproject.entities.ProductCategory;
@@ -11,36 +12,50 @@ import java.util.List;
 @RestController
 @AllArgsConstructor
 @CrossOrigin(origins = "*")
+@Slf4j
+
 public class ProductController {
 
     private final IProductService productService;
 
     @PostMapping("/product/{idStock}")
     Product addProduct(@RequestBody Product product,@PathVariable Long idStock){
+        log.warn("Adding product with ID {} to stock {}", product.getIdProduct(), idStock);
+
         return productService.addProduct(product,idStock);
     }
 
     @GetMapping("/product/{id}")
     Product retrieveProduct(@PathVariable Long id){
+        log.warn("Retrieving product with ID {}", id);
+
         return productService.retrieveProduct(id);
     }
 
     @GetMapping("/product")
     List<Product> retreiveAllProduct(){
+        log.warn("Retrieving all products");
         return productService.retreiveAllProduct();
     }
     @GetMapping("/product/stock/{id}")
     List<Product> retreiveProductStock(@PathVariable Long id){
+        log.warn("Retrieving products for stock with ID {}", id);
+
         return productService.retreiveProductStock(id);
     }
 
     @GetMapping("/productCategoy/{category}")
     List<Product> retrieveProductByCategory(@PathVariable ProductCategory category){
+        log.warn("Retrieving products by category: {}", category);
+
         return productService.retrieveProductByCategory(category);
     }
 
     @DeleteMapping("/product/{id}")
+
     void deleteProduct(@PathVariable Long id){
+        log.warn("Deleting product with ID {}", id);
+
         productService.deleteProduct(id);
     }
 }
